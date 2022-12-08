ee4a36ab0d10faa187f3b605b2043326213c3bfb
nati-soto <natimax24@gmail.com>
2022-12-08 11:59:45 -0300

    avances

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-const path = require('path');
-const fs = require('fs');
-
- console.log(path.resolve);
- path.resolve('./data/data.json');
- console.log( path.resolve(__dirname, './data/data.json') );
+  const fs = require('fs');
  
- console.log(path.join('./data/data.json'));
+ const path = require('path');
+ let destino = path.resolve(__dirname,'data/tareas.json');
+ console.log(__dirname);
+ 
+ let data = fs.readFileSync('./data/tareas.json');
+  
+let dataDescomprimida = JSON.parse(data);
+//console.log (dataDescomprimida);
 
- console.log(fs.readFileSync(rutaAlJson, {encoding: 'utf-8'}));
+switch (process.argv[2]){
+  case 'listar':
+    listar ();
+  break;
+  case undefined:
+console.log(`Atencion-Tienes que pasar a una accion\n las acciones disponibles son : listar`);
 
+  break;
+  default:
+  console.log(`No entiendo que quieres hacer\n las acciones disponovles son : listar `);
+  
+  break;
+}
+function listar(){
+  console.log('listado de tareas ');
+  console.log('--------------------')
+dataDescomprimida.forEach((element, i) => {
+   console.log( `${i + 1}. ${element.titulo} - ${element.estado}`);
+
+
+});
+
+}
